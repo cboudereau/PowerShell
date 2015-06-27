3390862b47de75da0056b63a44ced7599a12cb9e
cboudereau <cboudereau@gmail.com>
2015-06-27 11:51:42 +0200

    start build

---
+++ Teamcity/Get-TeamCityUri.ps1
@@ -0,0 +1,30 @@
+﻿function Get-TeamCityUri()
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(
+            Position=0, 
+            Mandatory=$true, 
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)
+        ]
+        $RelativePath
+    )
+
+    
+    Process
+    {
+        if($env:TEAMCITY)
+        {
+            $root = New-Object -TypeName System.Uri -ArgumentList @($env:TEAMCITY)
+            $controller = New-Object -TypeName System.Uri -ArgumentList @($root, $RelativePath)
+            
+            return $controller.AbsoluteUri
+        }
+        else
+        {
+            Write-Error "TeamCity Server is not set, you should use Set-Server to set the TeamCity server address"
+        }
+    }
+}
