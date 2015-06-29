4d65f15962ecae59463ec94552c270bc3c2aa7af
cboudereau <cboudereau@gmail.com>
2015-06-29 09:15:06 +0200

    add start build

---
+++ Teamcity/Get-Builds.ps1
@@ -0,0 +1,31 @@
+﻿function Get-Builds()
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Position=0, Mandatory=$false)]
+        [ValidateSet('ALL','SUCCESS','FAILURE','ERROR')]
+        $Status,
+
+        [Parameter(Position=1, Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+        $BuildType
+    )
+
+    Process
+    {
+        if($Status -and $Status -eq -not 'ALL')
+        {
+            
+        }
+        else
+        {
+            Write-Host "Choose None or ALL"
+        }
+        
+        $buildTypeId = $BuildType.id
+        $uri = "buildTypes/id:$buildTypeId/builds" | Get-TeamCityUri
+        $credential = Get-TeamCityCredential
+
+        ($uri | Get-FromJson $credential).build
+    }
+}
