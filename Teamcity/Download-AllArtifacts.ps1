9fd43402d7082a9cc97cf6d1ab4c144603614c90
cboudereau <cboudereau@gmail.com>
2015-07-01 19:12:44 +0200

    add

---
+++ Teamcity/Download-AllArtifacts.ps1
@@ -0,0 +1,25 @@
+﻿function Download-AllArtifacts()
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Position=0, Mandatory=$false)]
+        [string] $Directory = 'download',
+
+        [Parameter(Position=1, Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+        $Build
+    )
+
+    Process
+    {
+        $buildId = $Build.id
+        $buildTypeId = $Build.buildTypeId
+        $buildNumber = $Build.number
+        $selector = "repository/downloadAll/$buildTypeId/$buildId`:id/artifacts.zip"
+        $uri = $selector | Get-TeamCityUri -BasePath ""
+
+        $credential = Get-TeamCityCredential
+
+        Download-File -Credential $credential -Uri $uri -Folder "download/$buildTypeId/$buildNumber"
+    }
+}
---
+++ Teamcity/Download-File.ps1
@@ -0,0 +1,37 @@
+﻿function Download-File()
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Position=0, Mandatory=$true)]
+        [string] $Uri,
+
+        [Parameter(Position=1, Mandatory=$true)]
+        [pscredential] $Credential,
+
+        [Parameter(Position=2, Mandatory=$false)]
+        [string] $Folder = 'download'
+    )
+
+    Process
+    {
+        $webClient = New-Object System.Net.WebClient
+        $webClient.Credentials = $Credential
+        
+        if(!(Test-Path $Folder))
+        {
+            New-Item -Path $Folder -Type directory | Out-Null
+        }
+
+        $url = New-Object -TypeName System.Uri -ArgumentList @($Uri)
+        $fileName = [System.IO.Path]::GetFileName($url.LocalPath)
+
+        $target = Join-Path $Folder $fileName
+
+        $webClient.DownloadFile($Uri, $target)
+
+        $filePath = Resolve-Path $target
+
+        return $filePath
+    }
+}
