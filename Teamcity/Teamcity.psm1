b64d05635a7a73c21366f448cf8cbd5f99066bee
cboudereau <cboudereau@gmail.com>
2015-07-09 18:46:36 +0200

    fix

--- Teamcity/Teamcity.psm1
+++ Teamcity/Teamcity.psm1
@@ -1,6 +1,6 @@
 ﻿. $psScriptRoot\Download-AllArtifacts.ps1
 . $psScriptRoot\Download-File.ps1
-. $psScriptRoot\Get-Builds.ps1
+. $psScriptRoot\Get-Build.ps1
 . $psScriptRoot\Get-BuildStatus.ps1
 . $psScriptRoot\Get-BuildType.ps1
 . $psScriptRoot\Get-FromJson.ps1
