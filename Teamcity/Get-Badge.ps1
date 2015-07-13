cc4e8902c501cf7fba7f638513060cf1e45dd858
cboudereau <cboudereau@dev-mdossantos.siriona.com>
2015-07-13 14:58:19 +0200

    add badge and weblink

---
+++ Teamcity/Get-Badge.ps1
@@ -0,0 +1,22 @@
+﻿function Get-Badge()
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    Param
+    (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string] $Id,
+
+        [pscredential] $Credential
+    )
+
+    Process
+    {
+        $uri = Get-TeamCityUri -RelativePath "/buildType:(id:$Id)/statusIcon"
+        Write-Host $uri
+    }
+}
+
+Register-ParameterCompleter -CommandName 'Get-Badge' -ParameterName 'Id' -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    (Get-BuildType).buildType | Where-Object {$_.id -like "*$wordToComplete*"} | ForEach-Object { New-CompletionResult $_.id -ToolTip ('{0} ({1})' -f $_.name, $_.id )}
+}
---
+++ Teamcity/Get-TeamCityWebLink.ps1
@@ -0,0 +1,22 @@
+﻿function Get-TeamCityWebLink()
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    Param
+    (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string] $Id,
+
+        [pscredential] $Credential
+    )
+
+    Process
+    {
+        $uri = Get-TeamCityUri -RelativePath "/viewType.html?buildTypeId=$Id"
+        Write-Host $uri
+    }
+}
+
+Register-ParameterCompleter -CommandName 'Get-TeamCityWebLink' -ParameterName 'Id' -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    (Get-BuildType).buildType | Where-Object {$_.id -like "*$wordToComplete*"} | ForEach-Object { New-CompletionResult $_.id -ToolTip ('{0} ({1})' -f $_.name, $_.id )}
+}
