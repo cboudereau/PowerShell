8b1abf23b1a9b52ed6e192e836786dc32872c3b9
cboudereau <cboudereau@gmail.com>
2015-07-16 22:57:02 +0200

    project help files

--- Teamcity/New-TCProject.ps1
+++ Teamcity/New-TCProject.ps1
@@ -1,3 +1,19 @@
+<#
+    .SYNOPSIS
+    Create a new project
+
+    .DESCRIPTION
+    Given a solution under visual studio into the package manager console targeted a project, then the New-TCProject find the solution ascendant in order to create a project same called as solution name
+
+    .PARAMETER ParentId
+    aliased Id is the parent project where the project is created
+
+    .EXAMPLE
+    C:\PS> New-TCProject FSharp.Temporality
+
+    .EXAMPLE
+    C:\PS> Get-TCProject Net | New-TCProject Fsharp.Temporality | New-TCFromTemplate Net_Build
+#>
 function New-TCProject
 {
     [CmdletBinding(SupportsShouldProcess)]
